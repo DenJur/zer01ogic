8d6bc8a7d54d5c9b6df681e5dbdf4160e0ac886b
AndrewFlintUniversity <u1460287@unimail.hud.ac.uk>
2019-02-25 15:55:34 +0000

    Attempted to add drag and drop, currently not working as draggablenode is broken

--- zer01ogic-app/src/main/java/app/controllers/MainSceneController.java
+++ zer01ogic-app/src/main/java/app/controllers/MainSceneController.java
@@ -1,23 +1,43 @@
 package app.controllers;
 
+import app.dragdrop.DragContainer;
+import app.dragdrop.DragIcon;
+import app.dragdrop.DraggableNode;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Point2D;
+import javafx.scene.Cursor;
 import javafx.scene.Node;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainSceneController implements Initializable {
-
+    //GUI Components
     public GridPane gridpane_toolboxProperties;
     public GridPane gridpane_menu;
     public GridPane gridpane_main;
+    public Pane pane_canvas;
 
     private Node menuBarBuild;
     private Node menuBarSimulation;
     private Node toolbox;
+
+    //drag and drop
+    private DraggableNode mDragOverIcon = null;
+
+    private EventHandler<DragEvent> mIconDragOverRoot = null;
+    private EventHandler<DragEvent> mIconDragDropped = null;
+    private EventHandler<DragEvent> mIconDragOverRightPane = null;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -40,6 +60,8 @@ public class MainSceneController implements Initializable {
             FXMLLoader toolboxLoader = new FXMLLoader(getClass().getResource("/app/view/Toolbox.fxml"));
             toolboxLoader.setController(new ToolboxController());
             toolbox = toolboxLoader.load();
+            toolbox.setCursor(Cursor.HAND); //set the cursor to a hand when selecting
+            //TODO When selecting an item using drag + drop, perhaps a closed hand? ----------------------------------------------------------------------------------------------------
 
             //TODO load the selected item properties -----------------------------------------------------------------------------------------------------------------------------------
 
@@ -53,6 +75,19 @@ public class MainSceneController implements Initializable {
             gridpane_toolboxProperties.add(toolbox, 0, 0);
 
             //TODO add the selected item properties ------------------------------------------------------------------------------------------------------------------------------------
+
+
+            //TODO add drag and drop functionality -------------------------------------------------------------------------------------------------------------------------------------
+            //Add one icon that will be used for the drag-drop process
+            //This is added as a child to the root anchorpane so it can be visible
+            //on both sides of the split pane.
+            mDragOverIcon = new DraggableNode(); //-------------------------------------------------TODO INVESTIGATE WHY DRAGGABLE NODE BREAKS THE CODE---------------------------------
+
+            mDragOverIcon.setVisible(false);
+            mDragOverIcon.setOpacity(0.65);
+            gridpane_main.getChildren().add(mDragOverIcon);
+
+            buildDragHandlers();
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -72,5 +107,144 @@ public class MainSceneController implements Initializable {
             menuBarSimulation.setVisible(false);
             toolbox.setDisable(false);
         }
+    }
+
+    private void addDragDetection(DragIcon dragIcon) {
+
+        dragIcon.setOnDragDetected (new EventHandler <MouseEvent> () {
+
+            @Override
+            public void handle(MouseEvent event) {
+
+                // set drag event handlers on their respective objects
+                gridpane_main.setOnDragOver(mIconDragOverRoot);
+                pane_canvas.setOnDragOver(mIconDragOverRightPane);
+                pane_canvas.setOnDragDropped(mIconDragDropped);
+
+                // get a reference to the clicked DragIcon object
+                DragIcon icn = (DragIcon) event.getSource();
+
+                //begin drag ops
+                //mDragOverIcon.setType(icn.getType());
+                mDragOverIcon.relocateToPoint(new Point2D(event.getSceneX(), event.getSceneY()));
+
+                ClipboardContent content = new ClipboardContent();
+                DragContainer container = new DragContainer();
+
+                //container.addData ("type", mDragOverIcon.getType().toString());
+                content.put(DragContainer.AddNode, container);
+
+                mDragOverIcon.startDragAndDrop (TransferMode.ANY).setContent(content);
+                mDragOverIcon.setVisible(true);
+                mDragOverIcon.setMouseTransparent(true);
+                event.consume();
+            }
+        });
+    }
+
+    private void buildDragHandlers() {
+
+        //drag over transition to move widget form left pane to right pane
+        mIconDragOverRoot = new EventHandler <DragEvent>() {
+
+            @Override
+            public void handle(DragEvent event) {
+
+                Point2D p = pane_canvas.sceneToLocal(event.getSceneX(), event.getSceneY());
+
+                //turn on transfer mode and track in the right-pane's context 
+                //if (and only if) the mouse cursor falls within the right pane's bounds.
+                if (!pane_canvas.boundsInLocalProperty().get().contains(p)) {
+
+                    event.acceptTransferModes(TransferMode.ANY);
+                    mDragOverIcon.relocateToPoint(new Point2D(event.getSceneX(), event.getSceneY()));
+                    return;
+                }
+
+                event.consume();
+            }
+        };
+
+        mIconDragOverRightPane = new EventHandler <DragEvent> () {
+
+            @Override
+            public void handle(DragEvent event) {
+
+                event.acceptTransferModes(TransferMode.ANY);
+
+                //convert the mouse coordinates to scene coordinates,
+                //then convert back to coordinates that are relative to 
+                //the parent of mDragIcon.  Since mDragIcon is a child of the root
+                //pane, coodinates must be in the root pane's coordinate system to work
+                //properly.
+                mDragOverIcon.relocateToPoint(
+                        new Point2D(event.getSceneX(), event.getSceneY())
+                );
+                event.consume();
+            }
+        };
+
+        mIconDragDropped = new EventHandler <DragEvent> () {
+
+            @Override
+            public void handle(DragEvent event) {
+
+                DragContainer container =
+                        (DragContainer) event.getDragboard().getContent(DragContainer.AddNode);
+
+                container.addData("scene_coords",
+                        new Point2D(event.getSceneX(), event.getSceneY()));
+
+                ClipboardContent content = new ClipboardContent();
+                content.put(DragContainer.AddNode, container);
+
+                event.getDragboard().setContent(content);
+                event.setDropCompleted(true);
+            }
+        };
+
+        //this.setOnDragDone (new EventHandler <DragEvent> (){ -----------------------------------------------------------------------------------------------------------------------DELETE ME LATER
+
+        gridpane_main.setOnDragDone (new EventHandler <DragEvent> (){
+
+            @Override
+            public void handle (DragEvent event) {
+
+                pane_canvas.removeEventHandler(DragEvent.DRAG_OVER, mIconDragOverRightPane);
+                pane_canvas.removeEventHandler(DragEvent.DRAG_DROPPED, mIconDragDropped);
+                gridpane_main.removeEventHandler(DragEvent.DRAG_OVER, mIconDragOverRoot);
+
+                mDragOverIcon.setVisible(false);
+
+                DragContainer container =
+                        (DragContainer) event.getDragboard().getContent(DragContainer.AddNode);
+
+                if (container != null) {
+                    if (container.getValue("scene_coords") != null) {
+
+                        DraggableNode node = new DraggableNode();
+
+                        //node.setType(DragIconType.valueOf(container.getValue("type")));
+                        pane_canvas.getChildren().add(node);
+
+                        Point2D cursorPoint = container.getValue("scene_coords");
+
+                        node.relocateToPoint(
+                                new Point2D(cursorPoint.getX() - 32, cursorPoint.getY() - 32)
+                        );
+                    }
+                }
+
+                container =
+                        (DragContainer) event.getDragboard().getContent(DragContainer.DragNode);
+
+                if (container != null) {
+                    if (container.getValue("type") != null)
+                        System.out.println ("Moved node " + container.getValue("type"));
+                }
+
+                event.consume();
+            }
+        });
     }
 }
---
+++ zer01ogic-app/src/main/java/app/dragdrop/DraggableNode.java
@@ -0,0 +1,189 @@
+package app.dragdrop;
+
+import java.io.IOException;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Point2D;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.AnchorPane;
+
+public class DraggableNode extends AnchorPane {
+		
+//		@FXML AnchorPane root_pane;
+
+		private EventHandler <DragEvent> mContextDragOver;
+		private EventHandler <DragEvent> mContextDragDropped;
+		
+		private Point2D mDragOffset = new Point2D (0.0, 0.0);
+
+		@FXML private AnchorPane box;
+		
+//		@FXML private Label title_bar;
+//		@FXML private Label close_button;
+		
+		private final DraggableNode self;
+		
+		public DraggableNode() {
+			
+			FXMLLoader fxmlLoader = new FXMLLoader(
+					getClass().getResource("/AND.fxml")
+					);
+			
+			fxmlLoader.setRoot(this);
+			fxmlLoader.setController(this);
+			
+			self = this;
+			
+			try { 
+				fxmlLoader.load();
+	        
+			} catch (IOException exception) {
+			    throw new RuntimeException(exception);
+			}
+		}
+		
+		@FXML
+		private void initialize() {
+			box.scaleXProperty().set(1);
+			box.scaleYProperty().set(2);
+			buildNodeDragHandlers();
+		}
+		
+		public void relocateToPoint (Point2D p) {
+
+			//relocates the object to a point that has been converted to
+			//scene coordinates
+			Point2D localCoords = getParent().sceneToLocal(p);
+			
+			relocate ( 
+					(int) (localCoords.getX() - mDragOffset.getX()),
+					(int) (localCoords.getY() - mDragOffset.getY())
+				);
+		}
+
+		/*
+		public DragIconType getType () { return mType; }
+		
+		public void setType (DragIconType type) {
+			
+			mType = type;
+			
+			getStyleClass().clear();
+			getStyleClass().add("dragicon");
+			
+			switch (mType) {
+			
+			case blue:
+				getStyleClass().add("icon-blue");
+			break;
+
+			case red:
+				getStyleClass().add("icon-red");			
+			break;
+
+			case green:
+				getStyleClass().add("icon-green");
+			break;
+
+			case grey:
+				getStyleClass().add("icon-grey");
+			break;
+
+			case purple:
+				getStyleClass().add("icon-purple");
+			break;
+
+			case yellow:
+				getStyleClass().add("icon-yellow");
+			break;
+
+			case black:
+				getStyleClass().add("icon-black");
+			break;
+			
+			default:
+			break;
+			}
+		}
+		*/
+		public void buildNodeDragHandlers() {
+			
+			mContextDragOver = new EventHandler <DragEvent>() {
+
+				//dragover to handle node dragging in the right pane view
+				@Override
+				public void handle(DragEvent event) {		
+			
+					event.acceptTransferModes(TransferMode.ANY);				
+					relocateToPoint(new Point2D( event.getSceneX(), event.getSceneY()));
+
+					event.consume();
+				}
+			};
+			
+			//dragdrop for node dragging
+			mContextDragDropped = new EventHandler <DragEvent> () {
+		
+				@Override
+				public void handle(DragEvent event) {
+				
+					getParent().setOnDragOver(null);
+					getParent().setOnDragDropped(null);
+					
+					event.setDropCompleted(true);
+					
+					event.consume();
+				}
+			};
+			//close button click
+//			close_button.setOnMouseClicked( new EventHandler <MouseEvent> () {
+//
+//				@Override
+//				public void handle(MouseEvent event) {
+//					AnchorPane parent  = (AnchorPane) self.getParent();
+//					parent.getChildren().remove(self);
+//				}
+//
+//			});
+			
+			//drag detection for node dragging
+			box.setOnDragDetected ( new EventHandler <MouseEvent> () {
+
+				@Override
+				public void handle(MouseEvent event) {
+
+					getParent().setOnDragOver(null);
+					getParent().setOnDragDropped(null);
+
+					getParent().setOnDragOver (mContextDragOver);
+					getParent().setOnDragDropped (mContextDragDropped);
+
+	                //begin drag ops
+	                mDragOffset = new Point2D(event.getX(), event.getY());
+
+	                relocateToPoint(
+	                		new Point2D(event.getSceneX(), event.getSceneY())
+	                		);
+
+	                ClipboardContent content = new ClipboardContent();
+					DragContainer container = new DragContainer();
+
+					content.put(DragContainer.AddNode, container);
+
+	                startDragAndDrop (TransferMode.ANY).setContent(content);
+
+//	                box.scaleXProperty().set(box.scaleXProperty().get()-0.1);
+//	                box.scaleYProperty().set(box.scaleYProperty().get()-0.1);
+					box.rotateProperty().set(box.rotateProperty().getValue()+17);
+
+	                event.consume();
+				}
+
+			});
+		}		
+}
