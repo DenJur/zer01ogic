97b65f2cd55c44776a32b2be0a4d3d3b27c3243f
xDexiex <u1456914@unimail.hud.ac.uk>
2019-04-30 08:01:40 +0100

    Added tooltips to Simulation bar buttons and Build bar buttons

--- zer01ogic-app/src/main/java/app/controllers/MenuBarSimulationController.java
+++ zer01ogic-app/src/main/java/app/controllers/MenuBarSimulationController.java
@@ -6,6 +6,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,6 +15,21 @@ public class MenuBarSimulationController implements Initializable {
     private MenuBarSwitchLambda switchLambda;
 
     private CanvasController canvasController;
+
+    @FXML
+    private Button button_menu_toolbar_selection;
+
+    @FXML
+    private Button button_menu_toolbar_pointer;
+
+    @FXML
+    private Button button_menu_simulation_play;
+
+    @FXML
+    private Button button_menu_simulation_stop;
+
+    @FXML
+    private Button button_menu_mode_simulation;
 
     @FXML
     private Button button_menu_mode_build;
@@ -26,11 +42,21 @@ public class MenuBarSimulationController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        addTooltips();
         button_menu_mode_build.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 switchLambda.Switch();
             }
         });
+    }
+
+    private void addTooltips() {
+        Tooltip.install(button_menu_toolbar_selection, new Tooltip("Multiple Selection Tool"));
+        Tooltip.install(button_menu_toolbar_pointer, new Tooltip("Individual Selection Tool"));
+        Tooltip.install(button_menu_simulation_play, new Tooltip("Play Simulation"));
+        Tooltip.install(button_menu_simulation_stop, new Tooltip("Stop Simulation"));
+        Tooltip.install(button_menu_mode_simulation, new Tooltip("Simulation Mode"));
+        Tooltip.install(button_menu_mode_build, new Tooltip("Build Mode"));
     }
 }
