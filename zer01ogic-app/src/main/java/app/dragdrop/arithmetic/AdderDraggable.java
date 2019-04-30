bb1d0420927f5fc751ed598addfd4c054a49d4e4
DenJur <denis.jurockins02@gmail.com>
2019-04-30 10:29:44 +0100

    Adder, Subtractor simulation

--- zer01ogic-app/src/main/java/app/dragdrop/arithmetic/AdderDraggable.java
+++ zer01ogic-app/src/main/java/app/dragdrop/arithmetic/AdderDraggable.java
@@ -2,12 +2,15 @@ package app.dragdrop.arithmetic;
 
 import app.components.InputPin;
 import app.components.OutputPin;
+import app.components.Pin;
 import app.dragdrop.DraggableNode;
 import app.graphics.arithmetic.AdderGraphic;
+import app.models.WireLogic;
 import interfaces.circuits.ICircuitElementRegister;
 import interfaces.elements.IObservableValue;
 import javafx.geometry.Insets;
 import javafx.scene.layout.VBox;
+import simulation.arithmetic.Adder;
 
 import java.util.Collections;
 
@@ -17,6 +20,11 @@ import static app.graphics.GraphicsHelper.getPathStrokeWidth;
 public class AdderDraggable extends DraggableNode {
 
     private final AdderGraphic graphic;
+    private InputPin inputPinA;
+    private InputPin inputPinB;
+    private InputPin inputPinCIn;
+    private OutputPin outputPin;
+    private OutputPin outputPinCOut;
 
     public AdderDraggable() {
         this.graphic = new AdderGraphic();
@@ -31,34 +39,60 @@ public class AdderDraggable extends DraggableNode {
     @Override
     protected void createPins(double lineWidth) {
         //create 3 input and 2 output pins
-        InputPin inputPin1 = new InputPin(0, 18 + lineWidth / 2 + .5);
-        AnchorAll(inputPin1, 0, 0, 0, 0);
+        inputPinA = new InputPin(0, 18 + lineWidth / 2 + .5);
+        AnchorAll(inputPinA, 0, 0, 0, 0);
 
-        InputPin inputPin2 = new InputPin(0, 39 + lineWidth / 2 + .5);
-        AnchorAll(inputPin2, 0, 0, 0, 0);
+        inputPinB = new InputPin(0, 39 + lineWidth / 2 + .5);
+        AnchorAll(inputPinB, 0, 0, 0, 0);
 
-        InputPin inputPin3 = new InputPin(0, 60 + lineWidth / 2 + .5);
-        AnchorAll(inputPin3, 0, 0, 0, 0);
+        inputPinCIn = new InputPin(0, 60 + lineWidth / 2 + .5);
+        AnchorAll(inputPinCIn, 0, 0, 0, 0);
 
-        OutputPin outputPin1 = new OutputPin(164.0 + lineWidth, 29 + lineWidth / 2 + .5);
-        AnchorAll(outputPin1, 0, 0, 0, 0);
+        outputPin = new OutputPin(164.0 + lineWidth, 29 + lineWidth / 2 + .5);
+        AnchorAll(outputPin, 0, 0, 0, 0);
 
-        OutputPin outputPin2 = new OutputPin(164.0 + lineWidth, 50 + lineWidth / 2 + .5);
-        AnchorAll(outputPin2, 0, 0, 0, 0);
+        outputPinCOut = new OutputPin(164.0 + lineWidth, 50 + lineWidth / 2 + .5);
+        AnchorAll(outputPinCOut, 0, 0, 0, 0);
 
         //Add the pins to the DraggableNode's Pins ArrayList
-        Collections.addAll(super.pins, inputPin1, inputPin2, inputPin3, outputPin1, outputPin2);
-        this.getChildren().addAll(inputPin1, inputPin2, inputPin3, outputPin1, outputPin2);
+        Collections.addAll(super.pins, inputPinA, inputPinB, inputPinCIn, outputPin, outputPinCOut);
+        this.getChildren().addAll(inputPinA, inputPinB, inputPinCIn, outputPin, outputPinCOut);
     }
 
     @Override
     public IObservableValue getObservableValueForPin(OutputPin outputPin, ICircuitElementRegister register) {
-        return null;
+        if (outputPin == this.outputPin)
+            return ((Adder) register.getWorkingElementFor(this)).getOutput();
+        else
+            return ((Adder) register.getWorkingElementFor(this)).getBonusOutput();
     }
 
     @Override
     public void createLogicElement(ICircuitElementRegister register) {
+        Adder adder = new Adder((byte) 1);
+        register.addCircuitWorkingElement(this, adder);
+    }
 
+    @Override
+    public void connectLogicElementInputs(ICircuitElementRegister register) {
+        Adder adder = (Adder) register.getWorkingElementFor(this);
+        OutputPin outputPin = inputPinA.getConnectedWire().getOutputPin();
+        adder.setInputA(outputPin.getDraggableNode().getObservableValueForPin(outputPin, register));
+        outputPin = inputPinB.getConnectedWire().getOutputPin();
+        adder.setInputB(outputPin.getDraggableNode().getObservableValueForPin(outputPin, register));
+        outputPin = inputPinCIn.getConnectedWire().getOutputPin();
+        adder.setBonusInput(outputPin.getDraggableNode().getObservableValueForPin(outputPin, register));
+
+        for (Pin pin : pins) {
+            if (pin instanceof OutputPin) {
+                outputPin = (OutputPin) pin;
+                IObservableValue observableValue = getObservableValueForPin(outputPin, register);
+                for (WireLogic wireLogic : outputPin.getWiresLogic()) {
+                    observableValue.registerObserver(wireLogic);
+                    wireLogic.update(observableValue);
+                }
+            }
+        }
     }
 
     @Override
--- zer01ogic-app/src/main/java/app/dragdrop/arithmetic/SubtractorDraggable.java
+++ zer01ogic-app/src/main/java/app/dragdrop/arithmetic/SubtractorDraggable.java
@@ -2,13 +2,16 @@ package app.dragdrop.arithmetic;
 
 import app.components.InputPin;
 import app.components.OutputPin;
+import app.components.Pin;
 import app.dragdrop.DraggableNode;
 import app.graphics.arithmetic.SubtractorGraphic;
 import app.graphics.memory.SRFlipFlopGraphic;
+import app.models.WireLogic;
 import interfaces.circuits.ICircuitElementRegister;
 import interfaces.elements.IObservableValue;
 import javafx.geometry.Insets;
 import javafx.scene.layout.VBox;
+import simulation.arithmetic.Subtractor;
 
 import java.util.Collections;
 
@@ -18,6 +21,11 @@ import static app.graphics.GraphicsHelper.getPathStrokeWidth;
 public class SubtractorDraggable extends DraggableNode {
 
     private final SubtractorGraphic graphic;
+    private InputPin inputPinA;
+    private InputPin inputPinB;
+    private InputPin inputPinBIn;
+    private OutputPin outputPin;
+    private OutputPin outputPinBOut;
 
     public SubtractorDraggable() {
         this.graphic = new SubtractorGraphic();
@@ -32,34 +40,60 @@ public class SubtractorDraggable extends DraggableNode {
     @Override
     protected void createPins(double lineWidth) {
         //create 3 input and 2 output pins
-        InputPin inputPin1 = new InputPin(0, 18 + lineWidth / 2 + .5);
-        AnchorAll(inputPin1, 0, 0, 0, 0);
+        inputPinA = new InputPin(0, 18 + lineWidth / 2 + .5);
+        AnchorAll(inputPinA, 0, 0, 0, 0);
 
-        InputPin inputPin2 = new InputPin(0, 39 + lineWidth / 2 + .5);
-        AnchorAll(inputPin2, 0, 0, 0, 0);
+        inputPinB = new InputPin(0, 39 + lineWidth / 2 + .5);
+        AnchorAll(inputPinB, 0, 0, 0, 0);
 
-        InputPin inputPin3 = new InputPin(0, 60 + lineWidth / 2 + .5);
-        AnchorAll(inputPin3, 0, 0, 0, 0);
+        inputPinBIn = new InputPin(0, 60 + lineWidth / 2 + .5);
+        AnchorAll(inputPinBIn, 0, 0, 0, 0);
 
-        OutputPin outputPin1 = new OutputPin(164.0 + lineWidth, 29 + lineWidth / 2 + .5);
-        AnchorAll(outputPin1, 0, 0, 0, 0);
+        outputPin = new OutputPin(164.0 + lineWidth, 29 + lineWidth / 2 + .5);
+        AnchorAll(outputPin, 0, 0, 0, 0);
 
-        OutputPin outputPin2 = new OutputPin(164.0 + lineWidth, 50 + lineWidth / 2 + .5);
-        AnchorAll(outputPin2, 0, 0, 0, 0);
+        outputPinBOut = new OutputPin(164.0 + lineWidth, 50 + lineWidth / 2 + .5);
+        AnchorAll(outputPinBOut, 0, 0, 0, 0);
 
         //Add the pins to the DraggableNode's Pins ArrayList
-        Collections.addAll(super.pins, inputPin1, inputPin2, inputPin3, outputPin1, outputPin2);
-        this.getChildren().addAll(inputPin1, inputPin2, inputPin3, outputPin1, outputPin2);
+        Collections.addAll(super.pins, inputPinA, inputPinB, inputPinBIn, outputPin, outputPinBOut);
+        this.getChildren().addAll(inputPinA, inputPinB, inputPinBIn, outputPin, outputPinBOut);
     }
 
     @Override
     public IObservableValue getObservableValueForPin(OutputPin outputPin, ICircuitElementRegister register) {
-        return null;
+        if (outputPin == this.outputPin)
+            return ((Subtractor) register.getWorkingElementFor(this)).getOutput();
+        else
+            return ((Subtractor) register.getWorkingElementFor(this)).getBonusOutput();
     }
 
     @Override
     public void createLogicElement(ICircuitElementRegister register) {
+        Subtractor adder = new Subtractor((byte) 1);
+        register.addCircuitWorkingElement(this, adder);
+    }
 
+    @Override
+    public void connectLogicElementInputs(ICircuitElementRegister register) {
+        Subtractor adder = (Subtractor) register.getWorkingElementFor(this);
+        OutputPin outputPin = inputPinA.getConnectedWire().getOutputPin();
+        adder.setInputA(outputPin.getDraggableNode().getObservableValueForPin(outputPin, register));
+        outputPin = inputPinB.getConnectedWire().getOutputPin();
+        adder.setInputB(outputPin.getDraggableNode().getObservableValueForPin(outputPin, register));
+        outputPin = inputPinBIn.getConnectedWire().getOutputPin();
+        adder.setBonusInput(outputPin.getDraggableNode().getObservableValueForPin(outputPin, register));
+
+        for (Pin pin : pins) {
+            if (pin instanceof OutputPin) {
+                outputPin = (OutputPin) pin;
+                IObservableValue observableValue = getObservableValueForPin(outputPin, register);
+                for (WireLogic wireLogic : outputPin.getWiresLogic()) {
+                    observableValue.registerObserver(wireLogic);
+                    wireLogic.update(observableValue);
+                }
+            }
+        }
     }
 
     @Override
