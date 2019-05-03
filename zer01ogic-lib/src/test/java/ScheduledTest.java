ee026db20c76c337ab45e6fbb3abcc6d7d159a4b
DenJur <denis.jurockins02@gmail.com>
2019-05-03 08:34:18 +0100

    Restore Clock test

--- zer01ogic-lib/src/test/java/ScheduledTest.java
+++ zer01ogic-lib/src/test/java/ScheduledTest.java
@@ -1,50 +1,49 @@
 import interfaces.circuits.ICircuitElementRegister;
+import interfaces.circuits.ICircuitRunner;
 import interfaces.elements.ILogicElementFrontEnd;
+import org.junit.jupiter.api.Test;
+import simulation.DefaultCircuitBuilder;
 import simulation.io.Clock;
+import simulation.values.MultibitValue;
+import testHelpers.TestAndWrapper;
 import testHelpers.ValueUpdateCounter;
 
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class ScheduledTest {
-    //TODO write actual tests
-//    @Test
-//    public void Dirty() throws Exception {
-//        ClockWrapper clock=new ClockWrapper();
-//        TestAndWrapper and = new TestAndWrapper((byte) 1);
-//        and.addInputValue(clock.clock.getOutput());
-//        and.addInputValue(new MultibitValue(1, (byte) 1));
-//
-//        ICircuitRunner runner = new DefaultCircuitBuilder()
-//                .usingSingleThreadRunner()
-//                .buildWaitingCircuit()
-//                .addBusyScheduledExecutor()
-//                .build(Arrays.asList(and, clock));
-//        try {
-//            runner.startSimulation();
-//            for (int i=0;i<20; i++){
-////                long start = System.nanoTime();
-//                for(int l=0; l<1000;l++) {
-//                    runner.stop();
-//                    runner.reset();
-//                    runner.unpause();
-//                }
-//                Thread.sleep(1000);
-////                System.out.println("S time"+(System.nanoTime() - start) / 1000000);
-////                System.out.println(executor.count);
-//                System.out.println(and.counter.count);
-//                System.out.println(clock.counter.count);
-//            }
-//
-//        } catch (Exception e) {
-//            fail(e);
-//        }
-//        runner.stop();
-//    }
+    @Test
+    public void ClockAndTest() throws Exception {
+        ClockWrapper clock = new ClockWrapper();
+        TestAndWrapper and = new TestAndWrapper((byte) 1);
+        and.addInputValue(clock.clock.getOutput());
+        and.addInputValue(new MultibitValue(1, (byte) 1));
+
+        ICircuitRunner runner = new DefaultCircuitBuilder()
+                .usingSingleThreadRunner()
+                .buildWaitingCircuit()
+                .addBusyScheduledExecutor()
+                .build(Arrays.asList(and, clock));
+        try {
+            runner.startSimulation();
+            Thread.sleep(1010);
+            assertEquals(10, and.counter.count, "And was not updated 10 times");
+            assertEquals(10, clock.counter.count, "Clock was not updated 10 times");
+
+        } catch (Exception e) {
+            fail(e);
+        }
+        runner.stop();
+    }
 
     private class ClockWrapper implements ILogicElementFrontEnd {
         public Clock clock;
         public ValueUpdateCounter counter;
 
         ClockWrapper() {
-            clock = new Clock(1);
+            clock = new Clock(100);
             counter = new ValueUpdateCounter();
             clock.getOutput().registerObserver(counter);
         }
